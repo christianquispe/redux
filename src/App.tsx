7373894072b011a0d06aa6365571f4746a63cfe5
@christian <christianquispecamasca@gmail.com>
2019-11-24 12:50:43 -0500

    (#1) First Commit - Example

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import User from './pages/users/index';
+import './App.css';
+
+const App: React.FC = () =>{
+  return (
+    <div>
+      <User />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/users/AddUser.d.ts
@@ -0,0 +1,12 @@
+
+export interface AddUserParams {
+    userName: string;
+    userDOB: string;
+    userAge: string;
+    userGender: string;
+    userMobileNumber: string;
+    userAddress: string;
+    useraccountNumber: string;
+    userBankName: string;
+    userbankAddress: string;
+}
